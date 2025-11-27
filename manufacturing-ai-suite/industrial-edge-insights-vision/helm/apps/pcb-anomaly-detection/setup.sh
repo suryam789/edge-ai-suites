32212c1ede51b5cb772476cadb42aab683ac9c0d
Rajput, Sajeev <sajeev.rajput@intel.com>
2025-11-27 13:46:51 +0530

    pcb anomaly model upgraded to STFPM fp16

--- manufacturing-ai-suite/industrial-edge-insights-vision/helm/apps/pcb-anomaly-detection/setup.sh
+++ manufacturing-ai-suite/industrial-edge-insights-vision/helm/apps/pcb-anomaly-detection/setup.sh
@@ -3,7 +3,7 @@
 # Download artifacts for a specific sample application
 #   by calling respective app's setup.sh script
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
-MODEL_URL="https://github.com/open-edge-platform/edge-ai-resources/raw/ea2b2a606f45ae28e9a97e58a6488e7000dea263/models/FP16/pcb-anomaly-detection.zip"
+MODEL_URL="https://github.com/open-edge-platform/edge-ai-resources/raw/b6f2a1bc8212de186fd34ab2fb76fda69e80fa54/models/FP16/pcb-anomaly-detection.zip"
 VIDEO_URL="https://github.com/open-edge-platform/edge-ai-resources/raw/c13b8dbf23d514c2667d39b66615bd1400cb889d/videos/anomalib_pcb_test.avi"
 
 err() {
